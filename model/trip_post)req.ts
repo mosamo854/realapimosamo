62baaece290dea4c60319f97e403bdeba55353ed
mosamo854 <150302862+mosamo854@users.noreply.github.com>
2024-03-29 05:38:27 +0700

    Update trip_post)req.ts

--- model/trip_post)req.ts
+++ model/trip_post)req.ts
@@ -6,3 +6,18 @@ export interface TripGetResponse {
     img_user: string;
     type: string;
 }
+
+export interface VoteGetResponse {
+    vid: number;
+    iid: number;
+    uid: number;
+    date: Date;
+    score: number;
+}
+
+export interface ImageGetResponse {
+    iid: number;
+    uid: number;
+    img: string;
+    score_img: number;
+}
